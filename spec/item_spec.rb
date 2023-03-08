6838985a0872f30285de6555e193f7943f640ef4
Nick Sacco <nicksacco10@gmail.com>
2023-03-08 08:11:23 -0800

    Test: fix attribute test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -10,6 +10,6 @@ RSpec.describe Item do
   it 'exists and has attributes' do
     expect(@item1).to be_a(Item)
     expect(@item1.name).to eq('Peach Pie (Slice)')
-    expect(@item1.price).to eq(2.50)
+    expect(@item1.price).to eq(3.75)
   end
 end
