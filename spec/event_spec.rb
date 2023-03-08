f873bfe1046a0c94626a7c5e8e7849ad7d2235ba
Nick Sacco <nicksacco10@gmail.com>
2023-03-08 08:50:24 -0800

    Test: add_truck to event test

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -20,4 +20,9 @@ RSpec.describe Event do
     expect(@event.name).to eq("South Pearl Street Farmers Market")
     expect(@event.food_trucks).to eq([])
   end
+
+  it 'can add trucks to the event' do
+    @event.add_truck(@food_truck1)
+    expect(@event.food_trucks).to eq([@food_truck1])
+  end
 end
