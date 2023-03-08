4cfbb5a763db2b8c02ef0f3b2a28f0b4bee3dbc7
Nick Sacco <nicksacco10@gmail.com>
2023-03-08 08:06:01 -0800

    Test: Item init test

---
+++ spec/item_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require './lib/item'
+
+RSpec.describe Item do
+  before(:each) do
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+  end
+
+  it 'exists and has attributes' do
+    expect(@item1).to be_a(Item)
+  end
+end
