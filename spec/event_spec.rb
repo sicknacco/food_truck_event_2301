94d83887cb16e9c5c3af5b66492cd74d08efb872
Nick Sacco <nicksacco10@gmail.com>
2023-03-08 09:17:47 -0800

    Test: food trucks that sell tests

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -50,4 +50,23 @@ RSpec.describe Event do
     expect(@event.food_trucks).to eq([@food_truck1, @food_truck2, @food_truck3])
     expect(@event.food_truck_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
   end
+
+  it 'can determine what trucks sell a specific item' do
+    @event.add_food_truck(@food_truck1)
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+    
+    @event.add_food_truck(@food_truck2)
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+    
+    @event.add_food_truck(@food_truck3)
+    @food_truck3.stock(@item1, 65)
+
+    expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1, @food_truck3])
+    # expect(@event.food_trucks_that_sell(@item2)).to eq([@food_truck1])
+    # expect(@event.food_trucks_that_sell(@item3)).to eq([@food_truck2])
+    # expect(@event.food_trucks_that_sell(@item4)).to eq([@food_truck2])
+
+  end
 end
