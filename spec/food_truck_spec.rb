0883671abe4ac0d73ada7114168944f6a38cef19
Nick Sacco <nicksacco10@gmail.com>
2023-03-08 08:18:59 -0800

    Test: Initialize FoodTruck tests

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,3 +1,16 @@
 require 'rspec'
 require './lib/item'
 require './lib/food_truck'
+
+RSpec.describe FoodTruck do
+  before(:each) do
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+  end
+
+  it 'exists and has a name' do
+    expect(@food_truck).to be_a(FoodTruck)
+    # expect(@food_truck.name).to eq("Rocky Mountain Pies")
+  end
+end
