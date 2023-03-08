4d0197e14041ff03fabc48184d819e85ef8a59c8
Nick Sacco <nicksacco10@gmail.com>
2023-03-08 08:39:59 -0800

    Test: Initialize tests

---
+++ spec/event_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+require './lib/item'
+require './lib/food_truck'
+require './lib/event'
+
+RSpec.describe Event do
+  before(:each) do
+    @event = Event.new("South Pearl Street Farmers Market")
+  end
+
+  it 'exists and has attributes' do
+    expect(@event).to be_a(Event)
+    # expect(@event.name).to eq("South Pearl Street Farmers Market")
+    # expect(@event.food_trucks).to eq([])
+  end
+end
