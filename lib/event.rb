3cbdd2b5826c772d5b79899f38e1ab8d7b50d162
Nick Sacco <nicksacco10@gmail.com>
2023-03-08 08:51:38 -0800

    Feat: add truck method

--- lib/event.rb
+++ lib/event.rb
@@ -1,9 +1,13 @@
 class Event
   attr_reader :name,
               :food_trucks
-              
+
   def initialize(name)
     @name = name
     @food_trucks = []
+  end
+
+  def add_truck(truck)
+    @food_trucks << truck
   end
 end
